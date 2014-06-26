538961962884af27f3a815ac411980bd004bb2f6
max-gui <playstation.gui@gmail.com>
2014-06-26 12:47:52 +0800

    the first version to make a client for gcm server

---
+++ app/src/main/java/max/pushapp/GcmBroadcastReceiver.java
@@ -0,0 +1,22 @@
+package max.pushapp;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+/**
+ * Created by max on 6/25/14. the gcm receiver
+ */
+public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Explicitly specify that GcmIntentService will handle the intent.
+        ComponentName comp = new ComponentName(context.getPackageName(),
+                GcmIntentService.class.getName());
+        // Start the service, keeping the device awake while it is launching.
+        startWakefulService(context, (intent.setComponent(comp)));
+        setResultCode(Activity.RESULT_OK);
+    }
+}
